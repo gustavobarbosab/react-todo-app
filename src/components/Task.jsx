e92422f7ca40825617f16266d89e87e97750a12a
Gustavo Barbosa <gustavobarbosabarreto@gmail.com>
2022-09-04 05:55:17 -0300

    Changing color schema and improving ui

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,15 +1,26 @@
 import React from "react";
 import "./Task.css";
+import { MdDelete } from "react-icons/md";
+import { MdOutlineDone } from "react-icons/md";
 
 const Task = ({ task, onClick, handleRemoveTask }) => {
   return (
     <div
       className="task-container"
       style={task.completed ? { borderLeft: "6px solid chartreuse" } : {}}
-  >
-      <div onClick={() => onClick(task.id)}>{task.title}</div>
+      onClick={() => onClick(task.id)}
+    >
+      <div>{task.title}</div>
       <div className="buttons-container">
-        <button className="button-remove-task" onClick={() => handleRemoveTask(task.id)}>Remover</button>
+      <button style={task.completed ? { color: "green", display: "inline" } : {display: "none"}}>
+          <MdOutlineDone/>
+        </button>
+        <button
+          className="button-remove-task"
+          onClick={() => handleRemoveTask(task.id)}
+        >
+          <MdDelete/>
+        </button>
       </div>
     </div>
   );
