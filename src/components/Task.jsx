a2720a303e87cafc663a3c214dc8758594c9061a
Gustavo Barbosa <gustavobarbosabarreto@gmail.com>
2022-09-05 10:48:24 -0300

    Fixing color problem

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import "./Task.css";
-import { MdDelete } from "react-icons/md";
-import { MdOutlineDone } from "react-icons/md";
+import { MdDelete, MdInfoOutline, MdOutlineDone } from "react-icons/md";
 
 const Task = ({ task, onClick, handleRemoveTask }) => {
   return (
@@ -9,18 +8,23 @@ const Task = ({ task, onClick, handleRemoveTask }) => {
       className="task-container"
       style={task.completed ? { borderLeft: "6px solid chartreuse" } : {}}
     >
-      <div onClick={() => onClick(task.id)} className="task-title-container">{task.title}</div>
+      <div onClick={() => onClick(task.id)} className="task-title-container">
+        {task.title}
+      </div>
       <div className="buttons-container">
         <button
-          style={
-            task.completed
-              ? { color: "chartreuse", display: "inline" }
-              : { display: "none" }
-          }
+          className={task.completed ? "button-checked" : "button-uncheked"}
+          onClick={() => onClick(task.id)}
         >
           <MdOutlineDone />
         </button>
-        <button onClick={() => handleRemoveTask(task.id)}>
+        <button className="button-uncheked">
+          <MdInfoOutline />
+        </button>
+        <button
+          className="button-uncheked"
+          onClick={() => handleRemoveTask(task.id)}
+        >
           <MdDelete />
         </button>
       </div>
--- src/resources/Colors.js
+++
@@ -1,6 +0,0 @@
-export const COLORS = {
-    primary: '#b700ff',
-    white: '#FFF',
-}
-
-export default COLORS;
